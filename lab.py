6d7901e89d75e3d2deaa5f4eac04aae64cdaa3fc
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2025-10-31 11:19:09 +0100

    [ADD] Defined a function to count words in a textfile

--- lab.py
+++ lab.py
@@ -1,3 +1,5 @@
+
+
 #open a file
 #read it
 
@@ -7,3 +9,17 @@
 #txt file upload to s3 and get an email with "file xyz has xy words"
 
 
+def wordcount(textfile):
+    # set wordcount to 0 from start
+    c = 0
+    # Opening file in read only mode
+    with open(textfile, 'r') as file:
+        data = file.read()
+        w = data.split()
+        c += len(w)
+    print(c)
+
+# Testfile is in same folder
+text = 'testtext.txt'
+
+wordcount(text)
